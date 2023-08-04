c38b3aaa90ba5553e1eff29f20eedc1265c4114c
Mete <109483424+meteulken@users.noreply.github.com>
2023-08-04 21:54:19 +0300

    .

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0 , 0 , 0.1f);
     }
 }
