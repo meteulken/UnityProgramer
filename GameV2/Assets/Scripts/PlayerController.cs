56bc72cfaec3783b94c01a0b974f7ec7f7ca5578
Mete <109483424+meteulken@users.noreply.github.com>
2023-08-19 02:27:24 +0300

    .

---
+++ GameV2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange,transform.position.y,transform.position.z);
+        }
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange,transform.position.y,transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal"); // A, D, Left, Right
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
