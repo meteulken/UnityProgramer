ac63a5986692f8b0ee853db792249da4990c455f
Mete <109483424+meteulken@users.noreply.github.com>
2023-08-24 11:27:58 +0300

    .

---
+++ GameV2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float delay = 0.3f; // İstenilen gecikme süresi
+    private float lastTimePressed = -1.0f; // Son basılma zamanı
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time - lastTimePressed >= delay)
+            {
+                // Tuşa basıldığında ve gecikme süresi geçtiğinde burası çalışır
+                lastTimePressed = Time.time;
+                // Burada yapmak istediğiniz işlemleri gerçekleştirebilirsiniz
+                 Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+    }
+}
